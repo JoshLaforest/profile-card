23f761c39f4262be8f241b586f0cee08e5b6bb97
alexithewise <alexicoder@gmail.com>
2023-03-19 14:31:01 +0000

    edited tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,13 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  colors: {
+    'dark-cyan': 'hsl(185, 75%, 39%)',
+    'dark-blue': 'hsl(229, 23%, 23%)',
+    'grayish-blue': 'hsl(227, 10%, 46%)',
+    'dark-grey': 'hsl(0, 0%, 59%)',
+  },
+  fontFamily: {
+    sans: ['Kumbh Sans', 'sans-serif'],
+  }
 }
